505e40a4f4aa00c6e0c90bcd8413ccd17d5fbada
Thiago Peixoto <thiagopeixot13@gmail.com>
2021-03-08 22:30:14 -0300

    refactor e +doc das funções em Servers

--- source/Models/Servers.php
+++ source/Models/Servers.php
@@ -7,6 +7,13 @@ use Source\Models\Conect;
 class Servers extends Connect
 {
 
+    /**
+     * Cria um servidor no banco de dados
+     *
+     * @param array|object $content
+     * @return boolean
+     */
+
     public function createServer($content): bool
     {
         $content = (array)$content;
@@ -16,6 +23,14 @@ class Servers extends Connect
 
         return $result;
     }
+
+    /**
+     * Atualiza os valores de um servidor do banco de dados
+     *
+     * @param string $serverId
+     * @param array $content o conteúdo que vai ser alterado 
+     * @return boolean
+     */
 
     public function updateServer(string $serverId, array $content): bool
     {
@@ -28,6 +43,14 @@ class Servers extends Connect
         return $result;
     }
 
+
+    /**
+     * Deleta um servidor do banco de dados
+     *
+     * @param string $serverId
+     * @return boolean
+     */
+
     public function deleteServer(string $serverId): bool
     {
 
@@ -39,13 +62,21 @@ class Servers extends Connect
         return $result;
     }
 
-    public function findServer(string $serverId, array $columns = []): array
+    /**
+     * Encontra um servidor no banco e retorna um objeto, caso não exista retorna false
+     *
+     * @param string $serverId
+     * @param array $columns Caso seja necessário buscar colunas expecíficas
+     * @return object|bool
+     */
+
+    public function findServer(string $serverId, array $columns = [])
     {
         $result =
             $this->from('servers')
             ->where('server_id')->is($serverId)
             ->select($columns)
-            ->all();
+            ->first();
 
         return $result;
     }
@@ -73,26 +104,32 @@ class Servers extends Connect
         return $result;
     }
 
-    public function publishes(int $limit = null, array $colum = []): array
+    /**
+     * Retorna um array com os servidores que estão publicados
+     *
+     * @param integer $limit
+     * @param array $colum
+     * @return array
+     */
+
+    public function publishes(int $limit = 6, array $colum = []): array
     {
-        if ($limit) {
-            $result =
-                $this->from('servers')
-                ->where('published')->is(1)
-                ->orderBy('votes', 'desc')
-                ->limit($limit)
-                ->select($colum)
-                ->all();
-        } else {
-            $result =
-                $this->from('servers')
-                ->where('published')->is(1)
-                ->orderBy('votes', 'desc')
-                ->select($colum)
-                ->all();
-        }
+        $result =
+            $this->from('servers')
+            ->where('published')->is(1)
+            ->orderBy('votes', 'desc')
+            ->limit($limit)
+            ->select($colum)
+            ->all();
         return $result;
     }
+
+    /**
+     * Checa se o servidor existe e retorna um bool
+     *
+     * @param string $serverId
+     * @return boolean
+     */
 
     public function serverExists(string $serverId): bool
     {
@@ -100,16 +137,5 @@ class Servers extends Connect
             return false;
         }
         return true;
-    }
-
-    public function isPublished(string $serverId): bool
-    {
-        $result =
-            $this->from('servers')
-            ->where('server_id')->is($serverId)
-            ->select('published')
-            ->all();
-
-        return $result;
     }
 }
