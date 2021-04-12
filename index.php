9d743fb5c9e9b8dac0348e35c6e9e4f407abe4ac
Jovem-Blood <thiagopeixot13@gmail.com>
2021-04-12 18:19:58 -0300

    fix-typo and add search route

--- index.php
+++ index.php
@@ -4,55 +4,52 @@ session_start();
 
 use CoffeeCode\Router\Router;
 
-
 $router = new Router(URL_BASE);
+
 $router->namespace("Source\App");
 
 $router->group(null);
-
 $router->get('/', "Web:home");
-
 $router->get('/login', "Web:login");
-
 $router->get('/logout', "Web:logout");
-
 $router->get('/profile', "Web:profile");
-
 
 /*
  * Servers
- * */
+ */
 
 $router->group('servers');
-
 $router->get('/{serverId}', "Web:server");
-
 $router->get('/{serverId}/vote', "Web:vote");
-
 $router->get('/{serverId}/join', "Web:join");
-
 $router->group('config');
-
 $router->get('/{serverId}', "Web:config");
 $router->post('/{serverId}', "Web:form");
+
 /*
- * API
- */
+* API
+*/
+
 $router->group('api');
 $router->post('/test', "Api:test");
 $router->post('/rank', "Api:rank");
 $router->post('/create', "Api:create");
 $router->post('/update', "Api:update");
 $router->post('/delete', "Api:delete");
+
+/*
+* Search
+*/
+
+$router->group('s');
+$router->post('/{query}', "Web:search");
+
 /*
  * Errors and Dispatch
- */
+*/
 
 $router->group("ooops");
-
-
 $router->get("/{errcode}", "web:error");
-
 
 $router->dispatch();
 
